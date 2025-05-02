861d8f6dcd3ef553fb976d013ea289b371f48c53
stanislav <stanislav.orlovskiy@fabrique.studio>
2025-05-02 14:22:50 +0300

    Switch to hash history for GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 // Компоненты для маршрутов
 import TaskList from '../views/TaskList.vue';
@@ -124,7 +124,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 });
 
